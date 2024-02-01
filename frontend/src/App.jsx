8356e4536338ce088aa61cbecdbb25d601a812b4
Hamidou Mamadou <midou88530@gmail.com>
2024-02-01 16:39:01 +0100

    Add all the pages and add the filter for the south categorie

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
 import { Outlet } from "react-router-dom";
 
-
 import "./App.css";
-import NewsLetters from "./components/NewsLetters";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <NewsLetters />
       <main>
         <Outlet />
       </main>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
 import Homme from "./pages/Homme";
+import Femme from "./pages/Femme";
+import Enfant from "./pages/Enfant";
 
 const router = createBrowserRouter([
   {
     element: <App />,
     children: [
       {
-        path: "/",
-        //element: <Home />,
-      },
-      {
         path: "/homme",
         element: <Homme />,
+        children: [
+          {
+            path: "/homme/:souscategorie",
+            element: <Homme />,
+          },
+        ],
+      },
+      {
+        path: "/femme",
+        element: <Femme />,
+        children: [
+          {
+            path: "/femme/:souscategorie",
+            element: <Femme />,
+          },
+        ],
+      },
+      {
+        path: "/enfant",
+        element: <Enfant />,
+        children: [
+          {
+            path: "/enfant/:souscategorie",
+            element: <Enfant />,
+          },
+        ],
       },
     ],
   },
